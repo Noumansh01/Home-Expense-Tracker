f76ee36b6faa5047ccb32bb98083c836354f4a66
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-08-20 18:11:34 +0500

    app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import Sidebar from "./components/Sidebar"
 
+
+
 function App() {
   
 
   return (
-   
+    <>
+    <main className="w-full h-screen bg-sky-700">
+
    <Sidebar/>
+
+
+   </main>
+
+
+   </>
   )
 }
 
