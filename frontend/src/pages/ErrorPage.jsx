5711ff2ca483cc3ff6e0b7f5b5b9cdc577a343c3
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-08-22 16:01:05 +0500

    make error page

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
+const ErrorPage = ()=>{
+
+    const navigate = useNavigate();
+
+    // this func is to handle btn to get back in previous page by help of usenavigate hook
+    const handlebtn = ()=>{
+       navigate(-1);
+    };
+    return(
+        <>
+        <h1>404 Error Page</h1>
+        <button className="w-52 h-10 text-base text-white bg-black hover:text-lime-800 " onClick={handlebtn}>Click to go Previous Page</button>
+        </>
+    )
+}
+export default ErrorPage
