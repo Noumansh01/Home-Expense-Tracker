da29fdfbdfa96a4df9540c67d8cdc1251c9c9d95
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-08-28 21:09:56 +0500

    change text

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@
 
 const Signup = () => {
   return (
-    <div className="text-4xl text-blue-800"> THIS IS SIGNUP PAGE</div>
+    <div className="text-4xl text-blue-800"> THIS IS SIGNUP PAGE PLEASE ENTER DETAILS</div>
   )
 }
 
