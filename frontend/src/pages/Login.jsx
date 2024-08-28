4d87a2f88ab694527ef2752a46e1bb064a7c315e
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-08-28 21:09:03 +0500

    modify text

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@
 
 const Login = () => {
   return (
-    <div className="text-4xl text-blue-800"> THIS IS LOGIN PAGE</div>
+    <div className="text-4xl text-blue-800"> THIS IS LOGIN PAGE PLEASE ENTER CORRECT INFORMATION</div>
   )
 }
 
